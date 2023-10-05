a62b7b66ee55980eac7849138a997fd84e71fa00
vchemu <valerine.chemutai@student.moringaschool.com>
2023-10-06 01:20:55 +0300

    countdown

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
 # your code goes here!
+import time
+
+def countdown(i):
+    
+    while i:
+        print(f"{i} SECOND(S)!")
+        i -= 1
+    print("HAPPY NEW YEAR!")
+        
+
+def countdown_with_sleep(number):
+    
+    while number:
+        print(f"{number} SECOND(S)!")
+        number -= 1
+        time.sleep(1)
+
+    print("HAPPY NEW YEAR!")
+
+countdown_with_sleep(10)
